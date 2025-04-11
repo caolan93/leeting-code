a0aeec5dd2388bb8d5b39128af5953e21a736d56
caolan93 <caolan.fanning@gmail.com>
2025-04-10 20:56:37 -0400

    Time: 1 ms (81.09%) | Memory: 56.1 MB (78.29%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.ts
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.ts
@@ -1,13 +1,6 @@
 function intersection(nums1: number[], nums2: number[]): number[] {
     let set1 = new Set(nums1)
     let set2 = new Set(nums2)
-    let arr = []
-
-    Array.from(set1).map((num) => {
-        if(set2.has(num)){
-            arr.push(num)
-        }
-    })
-
-    return arr
+  
+    return [...set1].filter(num => set2.has(num));
 };
