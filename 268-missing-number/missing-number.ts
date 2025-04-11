383dd192ee152c85b9f31a8d1ef939da7f58b3e8
caolan93 <caolan.fanning@gmail.com>
2025-04-11 16:38:32 -0400

    Time: 1 ms (77.21%) | Memory: 55.8 MB (88.68%) - LeetSync

--- 268-missing-number/missing-number.ts
+++ 268-missing-number/missing-number.ts
@@ -1,5 +1,5 @@
 function missingNumber(nums: number[]): number {
 let expectedSum = (nums.length * (nums.length + 1)) / 2;
-let actualSum = nums.reduce((acc, val) => acc + val);
+let actualSum = nums.reduce((acc, val) => acc + val, 0);
 return expectedSum - actualSum;
 }
