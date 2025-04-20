9c942bd211206017e47c8f0238763fd6461a34a0
caolan93 <caolan.fanning@gmail.com>
2025-04-20 10:15:38 -0400

    Time: 21 ms (61.47%) | Memory: 80 MB (23.85%) - LeetSync

--- 219-contains-duplicate-ii/contains-duplicate-ii.ts
+++ 219-contains-duplicate-ii/contains-duplicate-ii.ts
@@ -1,6 +1,5 @@
 function containsNearbyDuplicate(nums: number[], k: number): boolean {
   let map = new Map<number, number>()
-  let distance:number = 0
 
   for(let i = 0; i <= nums.length; i++){
     if (map.has(nums[i])) {
