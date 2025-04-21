2701fd3d053cf0a1bb38c016b22763261f7b27dd
caolan93 <caolan.fanning@gmail.com>
2025-04-21 08:43:29 -0400

    Time: 3 ms (91.83%) | Memory: 61.6 MB (74.82%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.ts
+++ 13-roman-to-integer/roman-to-integer.ts
@@ -12,49 +12,15 @@ function romanToInt(s: string): number {
 
     let num: number = 0;
 
-    if (!s || s.length === 0) return num
+      for (let i = 0; i < s.length; i++) {
+        const current = hashMap[s[i]];
+        const next = hashMap[s[i + 1]];
 
-    for (let i = 0; i < s.length; i++) {
-        if (s[i] === 'V') {
-            if (s[i - 1] === 'I') {
-                num += 3
-            } else {
-                num += 5
-            }
-        } else if (s[i] === 'X') {
-            if (s[i - 1] === 'I') {
-                num += 8
-            } else {
-                num += 10
-            }
-        } else if (s[i] === 'L') {
-            if (s[i - 1] === 'X') {
-                num += 30
-            } else {
-                num += 50
-            }
-        } else if (s[i] === 'C') {
-            if (s[i - 1] === 'X') {
-                num += 80
-            } else {
-                num += 100
-            }
-        } else if (s[i] === 'D') {
-            if (s[i - 1] === 'C') {
-                num += 300
-            } else {
-                num += 500
-            }
-        } else if (s[i] === 'M') {
-            if (s[i - 1] === 'C') {
-                num += 800
-            } else {
-                num += 1000
-            }
+        if (next && current < next) {
+            num -= current;
         } else {
-            num += hashMap[s[i]]
+            num += current;
         }
-
     }
 
     return num
