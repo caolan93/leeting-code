606de24dc585d5f388ce352bff57f422d4495198
caolan93 <caolan.fanning@gmail.com>
2025-04-22 14:17:57 -0400

    Time: 7 ms (22.62%) | Memory: 58.4 MB (30.32%) - LeetSync

--- 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.ts
+++ 2053-check-if-all-characters-have-equal-number-of-occurrences/check-if-all-characters-have-equal-number-of-occurrences.ts
@@ -1,9 +1,10 @@
 function areOccurrencesEqual(s: string): boolean {
-    let map: Map<string, number> = new Map()
+    let map: Map<string, number> = new Map();
 
-    for(const char of s){
-        map.set(char, (map.get(char) || 0) + 1)
+    for (const char of s) {
+        map.set(char, (map.get(char) || 0) + 1);
     }
 
-    return [...map.values()].every(count => count === map.get(s[0]))
-};
+    const firstCount = [...map.values()][0];
+    return [...map.values()].every(count => count === firstCount);
+}
