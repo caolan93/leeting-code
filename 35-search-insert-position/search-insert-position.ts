6979411fc02774c310e6ea559ea5191ace5ab609
caolan93 <caolan.fanning@gmail.com>
2025-04-28 20:13:52 +0100

    Time: 0 ms (100.00%) | Memory: 55.9 MB (50.88%) - LeetSync

--- 35-search-insert-position/search-insert-position.ts
+++ 35-search-insert-position/search-insert-position.ts
@@ -1,8 +1,18 @@
 function searchInsert(nums: number[], target: number): number {
-    for (let i = 0; i < nums.length; i++) {
-        if (nums[i] >= target) {
-            return i;
+    if (!nums || nums.length === 0) return 0;
+    let leftPosition: number = 0;
+    let rightPosition: number = nums.length - 1;
+
+    while (leftPosition <= rightPosition) {
+        const mid = Math.floor((leftPosition + rightPosition) / 2);
+        if (nums[mid] === target) {
+            return mid;
+        } else if (nums[mid] < target) {
+            leftPosition = mid + 1;
+        } else {
+            rightPosition = mid - 1;
         }
     }
-    return nums.length;
+
+    return leftPosition
 }
