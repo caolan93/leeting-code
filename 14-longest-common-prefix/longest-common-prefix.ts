efdd30b08e8361db4ca8eb000d308a3a785b7eef
Caolan Fanning <caolan.fanning@gmail.com>
2026-04-01 08:11:18 +0100

    Time: 1 ms (64.96%) | Memory: 57 MB (30.55%) - LeetSync

---
+++ 14-longest-common-prefix/longest-common-prefix.ts
@@ -0,0 +1,13 @@
+function longestCommonPrefix(strs: string[]): string {
+    if (!strs.length) return ""
+
+    for(let i = 0; i < strs[0].length; i++){
+        for(let j = 0; j < strs.length; j++){
+            if(strs[j][i] !== strs[0][i]){
+                return strs[0].slice(0,i)
+            }
+        }
+    }
+
+    return strs[0]
+};
